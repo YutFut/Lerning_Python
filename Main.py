7a3cede25b504b624e8461cfde2d48f30d832639
YutFut <55690061+YutFut@users.noreply.github.com>
2020-06-21 20:55:48 +0300

    telegrambot

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
-print('Hello world')
+import telebot
+
+bot = telebot.TeleBot("1233774770:AAE66yGNxBdMqjiIyKprdCpukqlGfztLksQ")
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+	bot.reply_to(message, message.text)
+
+bot.polling( none_stop = True )
